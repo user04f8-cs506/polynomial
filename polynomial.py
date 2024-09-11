74e26662203cf3a149895b54c7c191e0a8c3a2c7
Clark, Nathan <Nathan.Clark@noblis.org>
2024-09-11 17:11:04 -0400

    starting part b (evaluate) with ABC

--- polynomial.py
+++ polynomial.py
@@ -1,18 +1,29 @@
-class X:
+from abc import ABC, abstractmethod
+
+class Evaluatable(ABC):
+    @abstractmethod
+    def evaluate(self, x):
+        raise NotImplementedError()
+
+
+class X(Evaluatable):
     def __init__(self):
         pass
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return x
 
-class Int:
+class Int(Evaluatable):
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
 
-class Add:
+class Add(Evaluatable):
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
@@ -20,7 +31,7 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
-class Mul:
+class Mul(Evaluatable):
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
@@ -36,3 +47,6 @@ class Mul:
     
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1))
